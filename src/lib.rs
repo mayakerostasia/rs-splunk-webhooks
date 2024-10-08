6911ccb7ed5920663073fadf425fa7e085c3624f
nwisemanII <nwiseman@bluebastion.net>
2024-10-08 15:53:27 -0400

    pre-compose test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
 use anyhow::Error;
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{http::StatusCode, routing::{get, post}, Json, Router};
 use bb_lib_http_listener::Server;
 use conf::config;
 
@@ -10,9 +14,21 @@ async fn root_handler() -> StatusCode {
     StatusCode::OK
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SplunkWebhook {
+    result: HashMap<String, Value>
+}
+
+async fn webhook_handler(Json(payload): Json<SplunkWebhook>) -> Result<StatusCode, (StatusCode, String)> {
+    eprintln!("Payload received -> \n {:#?}", payload);
+    Ok(StatusCode::OK)
+}
+
 pub async fn run_server() -> Result<(), Error> {
     let config = config()?;
-    let router = Router::new().route("/", get(root_handler));
+    let router = Router::new()
+        .route("/", get(root_handler))
+        .route("/", post(webhook_handler));
 
     let server = Server::new(&config.bind_addr);
     server.listen(Some(router)).await?;
