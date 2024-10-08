c5f699a21201ca9bb8e3a27a66c5a385704c57f9
nwisemanII <nwiseman@bluebastion.net>
2024-10-08 14:02:02 -0400

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,14 @@ use conf::config;
 
 mod conf;
 
-async fn root_handler() ->StatusCode {
+async fn root_handler() -> StatusCode {
     eprintln!("Root Handler HIT");
     StatusCode::OK
 }
 
 pub async fn run_server() -> Result<(), Error> {
     let config = config()?;
-    let router = Router::new()
-        .route("/", get(root_handler));
+    let router = Router::new().route("/", get(root_handler));
 
     let server = Server::new(&config.bind_addr);
     server.listen(Some(router)).await?;
