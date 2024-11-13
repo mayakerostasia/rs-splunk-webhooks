5e4480b5afcdf816b6996b48b73e7940aa95360d
nwisemanII <nwiseman@bluebastion.net>
2024-11-13 10:26:44 -0500

    add star

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 use anyhow::Error;
-use axum::{
-    routing::{get, post},
-    Router,
-};
+use axum::{routing::get, Router};
 use bb_lib_http_listener::Server;
 use bb_lib_surreal_client::{connect, setup, DbGuard};
 use handlers::{root_handler, webhook_handler};
@@ -23,9 +20,7 @@ pub async fn run_server() -> Result<(), Error> {
     let conf = &statics::CONF;
     let _db = connect_to_db().await?;
 
-    let router = Router::new()
-        .route("/", get(root_handler))
-        .route("/", post(webhook_handler));
+    let router = Router::new().route("/*", get(root_handler).post(webhook_handler));
 
     let server = Server::new(&conf.bind_addr);
     debug!("Server listening on {}", &conf.bind_addr);
