547f6e26093a5c1ea223627f0080c35476d79941
nwisemanII <nwiseman@bluebastion.net>
2024-11-14 13:32:39 -0500

    add trace layer

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use bb_lib_http_listener::Server;
 use bb_lib_surreal_client::{connect, setup, DbGuard};
 // use handlers::{root_handler, webhook_handler};
 use handlers::webhook_handler;
+use tower_http::trace::TraceLayer;
 use tracing::debug;
 
 mod conf;
@@ -21,7 +22,9 @@ pub async fn run_server() -> Result<(), Error> {
     let conf = &statics::CONF;
     let _db = connect_to_db().await?;
 
-    let router = Router::new().route("/", post(webhook_handler));
+    let router = Router::new()
+        .route("/", post(webhook_handler))
+        .layer(TraceLayer::new_for_http());
 
     let server = Server::new(&conf.bind_addr);
     debug!("Server listening on {}", &conf.bind_addr);
