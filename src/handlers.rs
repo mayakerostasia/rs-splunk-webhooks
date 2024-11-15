004c82a8e2e88a73a02362aac07a51001b79744b
nwisemanII <nwiseman@bluebastion.net>
2024-11-15 13:31:01 -0500

    Simple string payload

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,6 +1,6 @@
-use crate::schema::SplunkWebhook;
-use axum::{http::StatusCode, Json};
-use bb_lib_surreal_client::Storable;
+// use crate::schema::SplunkWebhook;
+use axum::http::{HeaderMap, Method, StatusCode};
+// use bb_lib_surreal_client::Storable;
 use tracing::debug;
 
 // pub async fn root_handler() -> StatusCode {
@@ -9,17 +9,25 @@ use tracing::debug;
 // }
 
 pub async fn webhook_handler(
-    Json(payload): Json<SplunkWebhook>,
+    method: Method,
+    headers: HeaderMap,
+    payload: String,
+    // Json(payload): Json<SplunkWebhook>,
 ) -> Result<StatusCode, (StatusCode, String)> {
+    eprintln!("Method: {}", method);
+    eprintln!("Headers: {:#?}", headers);
+
     debug!("Payload received -> \n {:#?}", payload);
-    match payload.save().await {
-        Ok(webhook) => {
-            debug!("Webhook Stored! -> {:#?}", webhook);
-            Ok(StatusCode::OK)
-        }
-        Err(e) => {
-            debug!("Error saving webhook ->\n {:#?}", e);
-            Err((StatusCode::BAD_REQUEST, e.to_string()))
-        }
-    }
+    // match payload.save().await {
+    //     Ok(webhook) => {
+    //         debug!("Webhook Stored! -> {:#?}", webhook);
+    //         Ok(StatusCode::OK)
+    //     }
+    //     Err(e) => {
+    //         debug!("Error saving webhook ->\n {:#?}", e);
+    //         Err((StatusCode::BAD_REQUEST, e.to_string()))
+    //     }
+    // }
+    //
+    Ok(StatusCode::OK)
 }
